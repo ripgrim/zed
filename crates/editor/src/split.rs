017183af7e7b6b45fccb08a85ad72f3fb965fe64
Jakub Konka <kubkon@jakubkonka.com>
2026-02-18 18:17:49 +0100

    buffer_diff: Reduce number of entity clones in base_text_buffer

--- crates/editor/src/split.rs
+++ crates/editor/src/split.rs
@@ -2079,9 +2079,16 @@ impl LhsEditor {
         let main_buffer = rhs_multibuffer_snapshot
             .buffer_for_excerpt(excerpt_id)
             .unwrap();
-        let base_text_buffer = diff.read(lhs_cx).base_text_buffer();
-        let diff_snapshot = diff.read(lhs_cx).snapshot(lhs_cx);
-        let base_text_buffer_snapshot = base_text_buffer.read(lhs_cx).snapshot();
+        let diff_snapshot;
+        let base_text_buffer_snapshot;
+        let remote_id;
+        {
+            let diff = diff.read(lhs_cx);
+            let base_text_buffer = diff.base_text_buffer().read(lhs_cx);
+            diff_snapshot = diff.snapshot(lhs_cx);
+            base_text_buffer_snapshot = base_text_buffer.snapshot();
+            remote_id = base_text_buffer.remote_id();
+        }
         let new = rhs_multibuffer
             .excerpts_for_buffer(main_buffer.remote_id(), lhs_cx)
             .into_iter()
@@ -2110,7 +2117,7 @@ impl LhsEditor {
 
         let lhs_result = lhs_multibuffer.update_path_excerpts_quietly(
             path_key,
-            base_text_buffer.clone(),
+            diff.read(lhs_cx).base_text_buffer().clone(),
             &base_text_buffer_snapshot,
             new,
             deferred_removes,
@@ -2118,7 +2125,7 @@ impl LhsEditor {
         );
         if !lhs_result.excerpt_ids.is_empty()
             && lhs_multibuffer
-                .diff_for(base_text_buffer.read(lhs_cx).remote_id())
+                .diff_for(remote_id)
                 .is_none_or(|old_diff| old_diff.entity_id() != diff.entity_id())
         {
             lhs_multibuffer.add_inverted_diff(diff, lhs_cx);
